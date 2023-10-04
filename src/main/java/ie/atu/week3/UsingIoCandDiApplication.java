be04313be2d21c965cd9a27b1ade5a68591e1b68
Paddy downey <G00392495@Atu.ie>
2023-10-04 14:25:51 +0100

    Made new class called user service

---
+++ src/main/java/ie/atu/week3/UsingIoCandDiApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingIoCandDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingIoCandDiApplication.class, args);
+    }
+
+}
