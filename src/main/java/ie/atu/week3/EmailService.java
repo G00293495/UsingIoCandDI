bd744d5e1197887a6fcc47d6c874599a5123c38b
Paddy downey <G00392495@Atu.ie>
2023-10-04 15:19:16 +0100

    Applied IoC and DI for newUser

---
+++ src/main/java/ie/atu/week3/EmailService.java
@@ -0,0 +1,11 @@
+package ie.atu.week3;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail(String to, String message){
+
+    }
+}
